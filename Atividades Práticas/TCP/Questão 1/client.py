2971903566d711cbc7a9e104c7c0719cccca4314
alissonv <alisson.vieira@tytotech.com.br>
2021-07-06 16:19:37 -0300

    PRATICA-TCP: Código inicial do server e do client

---
+++ Atividades Práticas/TCP/Questão 1/client.py
@@ -0,0 +1,54 @@
+import socket 
+import os
+
+ip = "127.0.0.1"
+port = 7000
+
+addr = (ip, port) 
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+client_socket.connect(addr)
+
+comandos = [
+    "EXIT",
+    "TIME",
+    "DATE",
+    "FILES",
+    "HELP",
+    "DOWN 'nomeArquivo'"
+]
+def main():
+    while True:
+        entrada = input("comando: ")
+        
+        # Lista os comandos
+        if(entrada == "HELP"):
+            for c in comandos:
+                print(c)
+            break
+
+        # Envia mensagem
+        client_socket.send(entrada.encode("utf-8"))
+        
+        # Envia a mensagem e fecha a conexão
+        if(entrada == "EXIT"):
+            client_socket.close()
+            break
+
+        # Retorna a hora do sistema
+        if(entrada == "TIME"):
+            pass
+        
+        # Retorna a data do sistema
+        if(entrada == "DATE"):
+            pass
+
+        # Recebe os arquivos da pasta padrão
+        if(entrada == "FILES"):
+            pass
+
+        # Faz o download de um arquivo
+        if((entrada.split())[0] == 'DOWN'):
+            pass         
+        
+main()
---
+++ Atividades Práticas/TCP/Questão 1/server.py
@@ -0,0 +1,63 @@
+import threading 
+import socket 
+
+host = ""
+port = 7000
+addr = (host,port) 
+
+serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+serv_socket.bind(addr)
+
+BUFFER_SIZE = 4
+
+def programa(ip, port, con): 
+    while True : 
+        # recebe a mensagem
+        msg = con.recv(1024)
+
+        # decodifica a mensagem
+        msg_str = msg.decode('utf-8')
+        
+        if(msg_str == 'EXIT'):
+            print('Digitou:', msg_str)
+            break
+
+        if(msg_str == "DATE"):
+            print('Digitou:', msg_str)
+
+        if(msg_str == "TIME"):
+            print('Digitou:', msg_str)
+
+        if(msg_str == "FILES"):
+            print('Digitou:', msg_str)
+
+        if((msg_str.split())[0] == 'DOWN'):
+            print('Digitou:', msg_str)
+
+def main():
+    vetorThreads = []
+
+    while 1:
+        # Limite de 1 conexão
+        serv_socket.listen(10)
+
+        # Servidor escuta as conexões
+        (con, (ip,port) ) = serv_socket.accept()
+        print('Conexão aceita!\n', con, ip, port)
+
+        # Cria e inicia uma thread para cada cliente que chega
+        thread = threading.Thread(target=programa, args=(ip, port, con, ))
+        thread.start()
+        
+        # Adiciona ao vetor de threads
+        vetorThreads.append(thread)
+        
+    # Aguarda todas as threads serem finalizadas
+    for t in vetorThreads: 
+        t.join()
+
+    # Fecha conexao
+    serv_socket.close()
+
+main()
