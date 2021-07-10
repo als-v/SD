281a72e468417fef5eac92fda1beac85caf6d1c4
als-v <alisson.v3@hotmail.com>
2021-07-10 11:58:42 -0300

    PRATICA-TCP: funcao addfile funcionando

--- Atividades Práticas/TCP/Questão 2/client.py
+++ Atividades Práticas/TCP/Questão 2/client.py
@@ -1,3 +1,4 @@
+import os
 import socket 
 
 ip = "127.0.0.1"
@@ -12,8 +13,50 @@ def main():
     while True:
         entrada = input("comando: ")
 
+        # Adicionar um novo arquivo
         if(entrada.split()[0] == "ADDFILE"):
-            pass
+            nomeArquivo = entrada.split()[1]
+            arquivos = os.listdir(path='./client_files')
+
+            if nomeArquivo in arquivos:
+                fileNameSize = str.encode(str(len(nomeArquivo)))
+
+                if len(fileNameSize) < 256:
+                    messageType = str.encode(str(1))
+                    commandIdentif = str.encode(str(1))
+                    
+                    client_socket.send(messageType)
+                    client_socket.send(commandIdentif)
+                    client_socket.send(fileNameSize)
+
+                    for nome in nomeArquivo:
+                        byte = str.encode(nome)
+                        client_socket.send(byte) 
+                    
+                    tamanhoArquivo = (os.stat('./client_files/' + nomeArquivo).st_size).to_bytes(4, "big")
+                    client_socket.send(tamanhoArquivo)
+
+                    arquivoBytes = b''
+                    with open('./client_files/' + nomeArquivo, 'rb') as file:
+                        byte = file.read(1)
+                        while byte != b'':
+                            client_socket.send(byte)
+                            byte = file.read(1)
+
+                    print('aguardando resposta....')
+                    resposta = client_socket.recv(3)
+                    print('resposta: ', resposta)
+
+                    if(int(resposta[0]) == 2 and int(resposta[1]) == 1):
+                        if(int(resposta[2]) == 1):
+                            print('Arquivo copiado com sucesso')
+                        elif(int(resposta[2]) == 2):
+                            print('Erro ao copiar arquivo')
+                else:
+                    print('O tamanho do nome do arquivo excedeu o limite de caracteres')
+            else:
+                print('O arquivo solicitado não existe')
+
 
         if(entrada.split()[0] == "DELETE"):
             pass
--- Atividades Práticas/TCP/Questão 2/server.py
+++ Atividades Práticas/TCP/Questão 2/server.py
@@ -1,3 +1,4 @@
+import os
 import threading 
 import socket 
 
@@ -11,8 +12,51 @@ serv_socket.bind(addr)
 
 def programa(ip, port, con): 
     while True : 
-        # Recebe a mensagem
-        pass
+        # Recebe o tipo da mensagem
+        messageType = int(con.recv(1).decode("utf-8"))
+        # Recebe o comando
+        commandIdentif = int(con.recv(1).decode("utf-8"))
+        # Recebe tamanho do nome do arquivo
+        fileNameSize = con.recv(1).decode("utf-8") 
+        # ADDFILE
+        if(messageType == 1 and commandIdentif == 1):
+            nomeArquivo = b''
+            for _ in range(int(fileNameSize)):
+                bytes = con.recv(1)
+                print('chegou: ', bytes) 
+                nomeArquivo += bytes
+
+            tamanhoArquivo = int.from_bytes(con.recv(4), byteorder='big')
+            
+            arquivo = b''
+            for _ in range(tamanhoArquivo):
+                bytes = con.recv(1)
+                print('chegou: ', bytes) 
+                arquivo += bytes
+            
+            with open('./server_files/' + nomeArquivo.decode('utf-8'), 'w+b') as file:
+                file.write(arquivo)
+
+            resposta = bytearray(3)
+            resposta[0] = 2
+            resposta[1] = 1
+            
+            arquivos = os.listdir(path='./server_files')
+            if nomeArquivo.decode('utf-8') in arquivos:
+                resposta[2] = 1
+            else:
+                resposta[2] = 2
+            
+            con.send(resposta)
+
+        if commandIdentif == 2:
+            pass
+        
+        if commandIdentif == 3:
+            pass
+        
+        if commandIdentif == 4:
+            pass
 
 def main():
     vetorThreads = []
