2271ec9fc40bad1402fb546836cf6d0f499709b0
JuanFSR <juanfrangel15@gmail.com>
2021-07-18 21:08:34 -0300

    PRATICA-UDP: Adicionando comentários na questão 2

--- Atividades Práticas/UDP/Questão 2/client.py
+++ Atividades Práticas/UDP/Questão 2/client.py
@@ -1,3 +1,11 @@
+'''
+    ### QUESTÃO 1 - UDP ###
+    # Autores: Juan e Alisson
+    # Data de criação:      15/07/2021
+    # Data de modificação:  16/07/2021
+    #Descrição: 
+'''
+
 import socket
 import threading
 import os
@@ -5,6 +13,7 @@ import hashlib
 import math
 import time
 
+#Ip e Porta que será utilizada para comunicação entre cliente e servidor
 ip = '127.0.0.1'
 port = 5001
 addr = (ip, port)
@@ -13,44 +22,68 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def enviaArquivo():
     while(True):
+        #Recebe como input o nome do arquivo que deseja realizar o upload
         nomeArquivo = input('Digite o nome do arquivo que deseja enviar: ')
+        #Todos os arquivos presentes na pasta padrão do cliente
         arquivos = os.listdir(path='./client_files')
 
+        #Percorre todos os arquivos presentes na pasta padrão do cliente e verifica se o arquivo que deseja fazer o upload 
+        #existe na pasta padrão do cliente
         if nomeArquivo in arquivos:
+
+            #Verifica se o nome do arquivo possui menos de 255 bytes
             if len(nomeArquivo) < 256:
+                #Atribui a variavel tamanhoArquivo o tamanho do arquivo que será feito o upload
                 tamanhoArquivo = os.stat('./client_files/' + nomeArquivo).st_size
+                #Divide o tamanho do arquivo por 1024 para saber quantas vezes terá que realizar o envio do arquivo para o server, já que mandamos 1024 bytes de cada vez
                 quantidadeEnvio = math.ceil(tamanhoArquivo/1024)
                 md5_hash = hashlib.md5()
 
+                #Abre o arquivo desejado no modo de leitura de bytes
                 with open("./client_files/a.txt", "rb") as file:
+                    #Realiza o checksum do arquivo
                     checksum = file.read()
                     md5_hash.update(checksum)
                     checksum = md5_hash.hexdigest()
 
+                    #Concatena o nome do arquivo, o tamano do arquivo e o checksum do arquivo que será enviado em uma só mensagem
                     msg = nomeArquivo + ';' + str(tamanhoArquivo) + ';' + checksum + ';' + str(quantidadeEnvio)
-                    # print(nomeArquivo, '\n', tamanhoArquivo, '\n', checksum, '\n', quantidadeEnvio)
+                    # Envia a mensagem, o ip e a porta para o server, encodando a mensagem ao enviar
                     sock.sendto(msg.encode('utf-8'), addr)
 
+                #Abre novamente o arquivo
                 with open("./client_files/a.txt", "rb") as file:
+                    #Lê 1024 bytes
                     byte = file.read(1024)
 
+                    #Lerá 1024 bytes de cada vez até que chegue ao final do arquivo
                     while byte != b'':
+                        #Envia o arquivo, 1024 bytes de cada vez, junto com o ip e a porta 
                         sock.sendto(byte, addr)
+                        #Lê os próximos 1024 bytes do arquivo
                         byte = file.read(1024)
                 
+                #Recebe uma mensagem como resposta do server, contém o status do upload, assim como o ip e a porta.
+                #Essa mensagem tem tamanho de um byte
                 dataReceive, addrReceive = sock.recvfrom(1)
 
+                #Verifica o status da mensagem
+                #Caso a mensagem de resposta seja 1, a operação foi bem sucedida
                 if dataReceive[0] == 1:
                     print('Operação realizada com sucesso!')
                 else:
+                    #Caso contrário, retornará 2 como resposta, indicando que ocorreu algum erro ao realizar o upload
                     print('Erro ao realizar a operação')
 
             else:
+                #Se o tamanho do arquivo possui mais do que 255 bytes, será retornado uma mensagem de erro
                 print('ERRO: O nome do arquivo exede o limite de caracteres')
         else:
+            #Caso o nome do arquivo não esteja presente na lista de arquivos, uma mensagem será retornada avisando que o arquivo não foi encontrado
             print('ERRO: Arquivo não encontrado')
 
 def main():
+    #Cria uma thread para realizar a operação de envio do arquivo
     enviarThread = threading.Thread(target=enviaArquivo)
     enviarThread.start()
 
--- Atividades Práticas/UDP/Questão 2/server.py
+++ Atividades Práticas/UDP/Questão 2/server.py
@@ -1,3 +1,11 @@
+'''
+    ### QUESTÃO 1 - UDP ###
+    # Autores: Juan e Alisson
+    # Data de criação:      15/07/2021
+    # Data de modificação:  16/07/2021
+    #Descrição: 
+'''
+
 import socket
 import hashlib
 import os
@@ -7,45 +15,63 @@ port = 5001
 addr = (ip, port)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#Escuta a porta e o IP definidos anteriormente
 sock.bind(addr)
 
 def recebeArquivo():
     while(True):
         arquivo = b''
 
+        #Recebe a mensagem do cliente que possui o nome do arquivo, tamanho do arquivo, o checksum e quantidade de vezes que o envio será executado
         data, addr = sock.recvfrom(1024)
+        #Realiza uma divisão da mensagem recebida, utilizando como identificador para realizar essa divisão o caractere ';'
+        #E também decodifica a mensagem
         msg = data.decode('utf-8').split(';')
 
         nomeArquivo = msg[0]
         tamanhoArquivo = msg[1]
         checksum = msg[2]
         quantidadeEnvio = msg[3]
-        # print('tamanhoNomeArquivo: ', tamanhoNomeArquivo, 'tamanhoArquivo: ', tamanhoArquivo, 'checksum: ', checksum, 'quantidadeEnvio: ', quantidadeEnvio, '\n')
         
+        #Realiza um for tendo como parâmetro a quantidade de vezes que ocorrerá o envio do arquivo
         for _ in range(int(quantidadeEnvio)):
+            #Recebe 1024 bytes do arquivo de cada vez, recendo também o ip e a porta de quem enviou 
             data, addr = sock.recvfrom(1024)
+            #Concatena os 1024 bytes recebidos
             arquivo += data
         
+        #Após receber todo arquivo é realizado um checksum
         md5_hash = hashlib.md5()
         novoChecksum = arquivo
         md5_hash.update(novoChecksum)
         novoChecksum = md5_hash.hexdigest()
+
+        #Será retornado como mensagem 1 se o checksum do arquivo recebido for igual ao checksum recebido do cliente
+        #Caso contrário será retornado 2
         retorno = bytearray(1)
         retorno[0] = 2
 
+        #Verifica se o checksum do arquivo é igual ao checksum enviado pelo cliente
         if novoChecksum == checksum:
+            #Caso seja igual, o arquivo recebido será criado na pasta padrão do server
             with open('./server_files/' + nomeArquivo, 'wb') as file:
                 file.write(arquivo)
             
             arquivos = os.listdir(path='./server_files')
             
+            #Caso ele existe na pasta padrão do server
+            #Será retornado uma mensagem com valor igual a 1
             if nomeArquivo in arquivos:
                 retorno[0] = 1
             else:
+                #Caso não exista, será retornado uma mensagem com o valor igual a 2
                 retorno[0] = 2
         else:
+            #Se o checksum for diferente, significa que ocorreu algum problema no upload do arquivo
+            #Retorna então uma mensagem com valor igual a 2
             retorno[0] = 2
 
+        #Envia a mensagem de resposta para o cliente, contendo também o ip e a porta
         sock.sendto(retorno, addr)
 
 def main():
