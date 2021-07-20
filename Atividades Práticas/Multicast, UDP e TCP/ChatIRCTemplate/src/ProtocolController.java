d86be90d6e2d526d8c03a3ccbe328325a0e9104f
als-v <alisson.v3@hotmail.com>
2021-07-20 15:39:44 -0300

    PRATICA-MULTICAST: terminado funções sendMessage

--- Atividades Práticas/Multicast, UDP e TCP/ChatIRCTemplate/src/ProtocolController.java
+++ Atividades Práticas/Multicast, UDP e TCP/ChatIRCTemplate/src/ProtocolController.java
@@ -36,12 +36,65 @@ public class ProtocolController {
     }
 
     public void send(String targetUser, String msg) throws IOException {
+        Byte type;
+        Message message = null;
+
+        // verifica se a mensagem é para todos ou se é 'privada'
+        if(targetUser.equals("Todos")){
+            if(msg.equals("JOIN")){
+                // JOIN: junta ao grupo todos
+                type = 1;
+            } else if(msg.equals("LEAVE")){
+                // LEAVE: deixa o grupo
+                type = 5;
+            } else {
+                // MSG: manda mensagem para todos
+                type = 3;
+            }
+
+            message = new Message(type, this.nick, msg);
+            sendMessageGroup(message, this.nick);
+
+        } else {
+            if(msg.equals("JOINAK")){
+                // JOINAK: resposta ao join
+                type = 2;
+            } else {
+                // MSGIDV: msg enviado 'privada'
+                type = 4;
+            }
+        
+            message = new Message(type, this.nick, msg);
+            sendMessage(message, onlineUsers.get(targetUser));
+        
+        }
     }
 
-    private void sendMessageGroup(Message msg) throws IOException {
+    private void sendMessageGroup(Message msg, String nick) throws IOException {
+        byte [] m = msg.getBytes();
+
+        /* Envia o tamanho da mensagem */
+        String tamMsg = Integer.toString(m.length);
+        DatagramPacket messageOut = new DatagramPacket(tamMsg.getBytes(), tamMsg.getBytes().length, group, mport);
+        udpSocket.send(messageOut);
+
+        /* Envia a mensagem */
+        messageOut = new DatagramPacket(m, m.length, group, mport);
+        udpSocket.send(messageOut);
+
     }
 
     private void sendMessage(Message msg, InetAddress target) throws IOException {
+        byte [] m = msg.getBytes();
+
+        /* Envia o tamanho da mensagem */
+        String tamMsg = Integer.toString(m.length);
+        DatagramPacket messageOut = new DatagramPacket(tamMsg.getBytes(), tamMsg.getBytes().length, target, uport);
+        udpSocket.send(messageOut);
+
+        /* Envia a mensagem */
+        messageOut = new DatagramPacket(m, m.length, target, uport);
+        udpSocket.send(messageOut);
     }
 
     public void join() throws IOException {
