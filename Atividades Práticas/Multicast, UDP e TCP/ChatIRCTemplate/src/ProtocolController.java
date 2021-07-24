0670a98833f7702156aa93c6190a8dbb12528a78
als-v <alisson.v3@hotmail.com>
2021-07-23 21:52:00 -0300

    .

--- Atividades Práticas/Multicast, UDP e TCP/ChatIRCTemplate/src/ProtocolController.java
+++ Atividades Práticas/Multicast, UDP e TCP/ChatIRCTemplate/src/ProtocolController.java
@@ -162,10 +162,10 @@ public class ProtocolController {
         // buffer = new byte[tamanho];
         // messageIn = new DatagramPacket(buffer, buffer.length);
         // multicastSocket.receive(messageIn);
-        System.out.println("receiveMulticastPAcket()");
         DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
         this.multicastSocket.receive(packet);
-
+        System.out.println("multicastSocket");
+        System.out.println(this.multicastSocket);
         this.processPacket(packet);
     }
 
