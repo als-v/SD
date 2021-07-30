11e9405a23115a24a5fe2bb4b00beaa50f7f2351
JuanFSR <juanfrangel15@gmail.com>
2021-07-30 20:29:05 -0300

    PRATICAS: Comentários terminados

--- Atividades Práticas/Multicast, UDP e TCP/ChatIRCTemplate/src/ProtocolController.java
+++ Atividades Práticas/Multicast, UDP e TCP/ChatIRCTemplate/src/ProtocolController.java
@@ -14,6 +14,9 @@ import java.io.*;
  * Gerencia o protocolo e o processamento das mensagens
  * 
  * @author rodrigo
+ * @author alisson
+ * @author juan
+
  */
 public class ProtocolController {
 
@@ -73,6 +76,8 @@ public class ProtocolController {
                 // MSGIDV: msg enviado 'privada'
                 type = 4;
             }
+            /* Cria uma nova mensagem */
+            /* Envia a mensagem para um usuário específico (targetUser) */
             message = new Message(type, this.nick, msg);
             sendMessage(message, onlineUsers.get(targetUser));
 
@@ -83,8 +88,9 @@ public class ProtocolController {
     private void sendMessageGroup(Message msg) throws IOException {
         byte[] m = msg.getBytes();
 
-        /* Envia o tamanho da mensagem */
+        /* Cria o datagrama que contém a mensagem,o tamanho da mensagem, o IP do grupo que deseja envia e a porta */
         DatagramPacket messageOut = new DatagramPacket(m, m.length, group, mport);
+        /* Envia em multicast */
         this.multicastSocket.send(messageOut);
     }
     
@@ -97,24 +103,36 @@ public class ProtocolController {
     }
     
     public void join() throws IOException {
+        /* Envia o endereço de multicasto do grupo para que realize um JOIN */
         this.multicastSocket.joinGroup(group);
         
+        /* Cria uma mensagem, enviando o tipo 1 (JOIN) e o nick de quem está enviando a mensagem */
         Byte type = 1;
         Message message = new Message(type, this.nick, "");
         
+        /* Envia a mensagem para todos (em multicast) */
         this.sendMessageGroup(message);
     }
     
     public void leave() throws IOException {
+        /* Método invocado caso queira sair do chat*/
+
+        /* Crian uma mensagem que tem o tipo 5 (LEAVE) e o nick de quem sairá do chat */
         Byte type = 5;
         Message message = new Message(type, this.nick, "");
+
+
+        /* Envia uma mensagem com o tipo 5 para ser retirado da lista de online */
         this.sendMessageGroup(message);
         
+        /* Envia uma mensagem multicast */
+        /* Chama o método leaveGroup enviando o IP de multicast utilizado para o grupo */
         this.multicastSocket.leaveGroup(group);
         close();
     }
     
     public void close() throws IOException {
+        /* Fecha os sockets */
         if (udpSocket != null)
         udpSocket.close();
         if (multicastSocket != null)
@@ -146,7 +164,7 @@ public class ProtocolController {
                 /* remove o apelido e endereço da lista de suários ativos */
                 this.onlineUsers.remove(senderNick);
             } else if (message.getType() == 6) {
-
+                /* Percorre o diretório definido anteriormente, adicionando o nome dos arquivos na variável arq (separando os nomes por \n)*/
                 File files = new File(this.diretorio);
                 File listaFiles[] = files.listFiles();
                 File arquivos = null;
