8ecf3913ed85fe598feab1e1928779d7c56ab170
JuanFSR <juanfrangel15@gmail.com>
2021-08-09 21:18:52 -0300

    PRATICAS: Atividade de gRPC

---
+++ Atividades Práticas/gRPC/grpc_java_helloworld/src/main/java/ServiceDatabase.java
@@ -0,0 +1,17 @@
+
+import io.grpc.stub.StreamObserver;
+
+/**
+ *
+ * @author rodrigo
+ */
+
+public class ServiceDatabase extends ServiceDatabaseGrpc.ServiceDatabaseImplBase {
+
+    @Override
+    public void GerenciaNotas(Request request, StreamObserver<Response> responseObserver) {
+
+    }
+    
+    
+}
